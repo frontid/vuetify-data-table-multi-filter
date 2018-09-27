e01027822e61becfcbae9fafa8f7b8b875e5c988
Marcelo Tosco @capynet <marcelo@front.id>
2018-09-27 15:33:50 +0200

    init.

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-content>
+      <InvoicesTable/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import InvoicesTable from './components/InvoicesTable'
+
+export default {
+  name: 'App',
+  components: {
+    InvoicesTable
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
